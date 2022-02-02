eb99494da53b0c78c8b7fd4f7cadc835ae7e13c7
itssweetrain <artraine@gmail.com>
2022-02-02 22:54:26 +0900

    edit: profile image

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "androboy's devlog",
   description: "Hello :) 삐리삐리뽀로뽀로",
   author: "Danbi",
-  siteUrl: "https://itssweetrain.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://itssweetrain.netlify.app/",
   links: {
     github: "https://github.com/itssweetrain",
     linkedIn: "",
